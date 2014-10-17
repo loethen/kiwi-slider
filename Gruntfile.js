8f65a418374126bc672311516fdb40331c8ad753
loethen <loethen.hs@gmail.com>
2014-10-17 15:23:53 +0800

    steps

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,7 +162,7 @@ module.exports = function (grunt) {
             },
             server: {
                 options: {
-                    debugInfo: true
+                    debugInfo: false
                 }
             }
         },
